7073d495ed8330e92008b2289babd8d27668964e
hardik355 <kapspatel355@gmail.com>
2019-11-14 14:51:18 +0530

    task controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
   
-  #project Routes 
-  get   '/projects', to: 'projects#index'
-  post  '/projects',  to: 'projects#create'
-  get   '/projects/new', to: 'projects#new'
-  get   '/projects/:id/', to: 'projects#show', as: 'project'
-  get   '/projects/:id/edit', to: 'projects#edit', as: 'edit_project'
-  put   '/projects/:id', to: 'projects#update'
-  post '/projects/:id', to: 'projects#update'
-  delete '/projects/:id', to: 'projects#destroy'
-  # resource :projects
-
   #admin route
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -27,5 +16,25 @@ Rails.application.routes.draw do
       root to: "devise/sessions#new"
     end
   end
+
+  #project Routes 
+  get   '/projects', to: 'projects#index'
+  post  '/projects',  to: 'projects#create'
+  get   '/projects/new', to: 'projects#new'
+  get   '/projects/:id/', to: 'projects#show', as: 'project'
+  get   '/projects/:id/edit', to: 'projects#edit', as: 'edit_project'
+  put   '/projects/:id', to: 'projects#update'
+  post '/projects/:id', to: 'projects#update'
+  delete '/projects/:id', to: 'projects#destroy'
+
+  # Task Custom Route 
+  get   '/tasks', to: 'tasks#index'
+  post  '/tasks',  to: 'tasks#create'
+  get   '/tasks/new', to: 'tasks#new'
+  get   '/tasks/:id/', to: 'tasks#show', as: 'task'
+  get   '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  put   '/tasks/:id', to: 'tasks#update'
+  post '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 
 end
